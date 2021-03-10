f44c904a27e25feb0de100baa4afc2cc13829a61
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-03-10 02:15:52 -0300

    Classe Department pacote entities

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department depart = new Department(6, "Gerente de TI");
+		System.out.println(depart);
+
+	}
+
+}
